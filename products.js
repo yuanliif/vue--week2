ad36838fbc9fd6156b4d9e5375b9c3f566d559fa
yuanliif <yuanwork4027@gmail.com>
2022-01-19 10:40:44 +0800

    first commit

---
+++ products.js
@@ -0,0 +1,46 @@
+import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js";
+
+createApp({
+  data() {
+    return {
+      apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+      apiPath: 'pastelsy',
+      products: [],
+      tempProduct: {},
+    }
+  },
+  methods: {
+    checkAdmin() {
+      const url = `${this.apiUrl}/api/user/check`;
+      axios.post(url)
+        .then(() => {
+          this.getData();
+        })
+        .catch((err) => {
+          alert(err.response.data.message)
+          window.location = 'index.html';
+        })
+    },
+    getData() {
+      const url = `${this.apiUrl}/api/${this.apiPath}/admin/products`;
+      axios.get(url)
+        .then((response) => {
+          console.log(response)
+          this.products = response.data.products;
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        })
+    },
+    openProduct(item) {
+      this.tempProduct = item;
+    }
+  },
+  mounted() {
+    // 取出 Token
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)yuanToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    axios.defaults.headers.common.Authorization = token;
+
+    this.checkAdmin()
+  }
+}).mount('#app');
