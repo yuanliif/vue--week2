bfeafffca66d752333cd87e21546dd74bfedd076
yuanliif <yuanwork4027@gmail.com>
2022-01-20 00:59:55 +0800

    products api

--- products.js
+++ products.js
@@ -1,4 +1,17 @@
 import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js";
+const product = {
+  data: {
+    title: '[賣]動物園造型衣服3',
+    category: '衣服2',
+    origin_price: 100,
+    price: 300,
+    unit: '個',
+    description: 'Sit down please 名設計師設計',
+    content: '這是內容',
+    is_enabled: 1,
+    imageUrl: 'https://images.unsplash.com/photo-1573662012516-5cb4399006e7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80'
+  }
+}
 
 createApp({
   data() {
@@ -32,8 +45,28 @@ createApp({
           alert(err.response.data.message);
         })
     },
-    openProduct(item) {
+    getDataInfo(item) {
       this.tempProduct = item;
+    },
+    addData() {
+      axios.post(`${this.apiUrl}/api/${this.apiPath}/admin/product`, product)
+        .then((res) => {
+          console.log(res.data)
+          this.getData()
+        })
+        .catch(error => {
+          console.dir(error)
+        })
+    },
+    deleteData(item) {
+      axios.delete(`${this.apiUrl}/api/${this.apiPath}/admin/product/${item.id}`)
+      .then((res) => {
+        console.log(res.data)
+        this.getData()
+      })
+      .catch(error => {
+        console.dir(error)
+      })
     }
   },
   mounted() {
